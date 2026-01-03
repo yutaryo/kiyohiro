6267a641a5a22cb00a5327b6204587490a5c53ba
ゆたりょお <yutario8888@gmail.com>
2026-01-04 03:01:45 +0900

    Initialize React application in index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'; // App.jsxを読み込む
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
