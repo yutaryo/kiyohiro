f17f73aaf2dc23cfe8c7fb7caf446cb34300cdd2
ゆたりょお <yutario8888@gmail.com>
2026-01-04 03:21:53 +0900

    Fix import statement for App component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // ここは './App' のままでOK（.js も .jsx も探してくれます）
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
